e27856844c309a0b754570b635fef35d88632764
jemsley canela <jemscanela@gmail.com>
2018-04-05 16:08:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string.toLowerCase() && string.toLowerCase()
-  != "i love you, grandma"){
+  if(string.toLowerCase()){
     return "I can't hear you!"
   }
   if(string.toUpperCase()){
